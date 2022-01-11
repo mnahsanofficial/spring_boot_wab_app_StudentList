33e5f9dabed41fb0cce8415209ff1e943244e1cc
mnahsanofficial <76871125+mnahsanofficial@users.noreply.github.com>
2022-01-11 16:50:41 +0600

    done

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,82 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/students/")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping("showForm")
+    public String showStudentForm(Student student){
+        return "add-student";
+    }
+    
+    @GetMapping("list")
+    public String students(Model model){
+        model.addAttribute("students", this.studentRepository.findAll());
+        return "index";
+
+    }
+
+    @PostMapping("add")
+    public String addStudent(@Validated Student student,BindingResult result,Model model){
+
+        if(result.hasErrors()){
+            return "add-student";
+
+        }
+        this.studentRepository.save(student);
+        return "redirect:list";
+    }
+
+    @GetMapping("edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id,Model model){
+        Student student=this.studentRepository.findById(id)
+        .orElseThrow(()-> new IllegalArgumentException("Invalid Student is :"+ id));
+
+        model.addAttribute("student", student);
+        return "update-student";
+
+    }
+
+    @GetMapping("update/{id}")
+    public String updateStudent(@PathVariable("id") long id,@Validated Student student,BindingResult result,Model model){
+        if(result.hasErrors()){
+            student.setId(id);
+            return "update-student";
+        }
+
+        //updatestudent
+        studentRepository.save(student);
+        //get all students (with updates)
+        model.addAttribute("students", this.studentRepository.findAll());
+        return "index";
+
+    }
+    @GetMapping("delete/{id}")
+	public String deleteStudent(@PathVariable ("id") long id, Model model) {
+		
+		Student student = this.studentRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid student id : " + id));
+		
+		this.studentRepository.delete(student);
+		model.addAttribute("students", this.studentRepository.findAll());
+		return "index";
+		
+	}
+
+}
